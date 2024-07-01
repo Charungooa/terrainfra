81ea740993b50dd1625390a5608aac0be3e7982c
v-2amitc <v-2amitc@hotmail.com>
2024-07-01 11:57:50 +0530

    updated the correct name of storage account

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "azurerm" {
         resource_group_name = "demo_amit_rg"
-        storage_account_name = "tfstatestorage82"
+        storage_account_name = "tfstatestorage48"
         container_name = "tfstate-cont"
         key = "mydemo.terraform.tfstate"
     }
